41b60278fd9b49476164012380ffa66a70d5bac7
Sagar Yadav <sagaryadav17@outlook.com>
2023-05-20 11:01:36 +0530

    CRUD added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
 dotenv.config({ path: "./.env" });
+
+const server = express();
+
+server.use(bodyParser.json());
 
 const Pool = require("pg").Pool;
 
@@ -12,47 +18,155 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-const server = express();
-
-server.get("/expense/", (req, res) => {
-  pool.query(
-    "SELECT * from app1_expense LEFT join app1_category on app1_expense.category_id = app1_category.id ORDER BY timestamp DESC",
-    (error, results) => {
+function run_query(query) {
+  return new Promise((resolve, reject) => {
+    pool.query(query, (error, results) => {
       if (error) {
-        throw error;
+        reject(error);
       }
+      resolve(results);
+    });
+  });
+}
 
-      res.status(200).json(results.rows);
+// Function to validate datetime string format
+function isValidDateTimeString(datetimeString) {
+  const regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+  return regex.test(datetimeString);
+}
+
+server.get("/expense/", async (req, res) => {
+  const { start_date, end_date } = req.query;
+
+  let query = `
+    SELECT expense.id::int, expense.timestamp, expense.amount, category.name AS category_name
+    FROM expense
+    LEFT JOIN category ON expense.category_id = category.id
+  `;
+
+  // Add filters if start_date and/or end_date are provided
+  if (start_date && end_date) {
+    query += ` WHERE timestamp::date >= '${start_date}' AND timestamp::date <= '${end_date}'`;
+  } else if (start_date) {
+    query += ` WHERE timestamp::date >= '${start_date}'`;
+  } else if (end_date) {
+    query += ` WHERE timestamp::date <= '${end_date}'`;
+  }
+
+  query += " ORDER BY timestamp DESC";
+
+  try {
+    const data = await run_query(query);
+    res.status(200).json(data.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+server.post("/expense/", async (req, res) => {
+  const { timestamp, amount, description, category_id } = req.body;
+
+  if (
+    typeof timestamp !== "string" ||
+    !isValidDateTimeString(timestamp) ||
+    typeof amount !== "number" ||
+    typeof description !== "string" ||
+    typeof category_id !== "number"
+  ) {
+    res.status(400).json({ error: "Invalid request body format." });
+    return;
+  }
+
+  const query = `INSERT INTO expense(timestamp, amount, description, category_id) VALUES('${timestamp}', '${amount}', '${description}', '${category_id}')`;
+
+  try {
+    await run_query(query);
+    res.status(201).json({ message: "Expense created successfully." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+server.get("/expense/:id/", async (req, res) => {
+  const expenseId = req.params.id;
+
+  let query = `
+    SELECT expense.id::int, expense.timestamp, expense.amount, expense.description, category.name AS category_name
+    FROM expense
+    LEFT JOIN category ON expense.category_id = category.id
+    WHERE expense.id = ${expenseId} LIMIT 1
+  `;
+  try {
+    const data = await run_query(query);
+    if (data.rows.length === 0) {
+      res.status(404).json({ error: "Expense not found." });
+    } else {
+      const expense = data.rows[0];
+      res.status(200).json(expense);
     }
-  );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-server.post("/expense/", (req, res) => {
-  const query =
-    "INSERT INTO app1_expense(timestamp,amount,description,category_id) VALUES('2023-05-20 01:55:40','2000','dometing',1)";
-  res.status(201).json();
+server.patch("/expense/:id/", async (req, res) => {
+  const expenseId = req.params.id;
+  const { amount, timestamp, description, category_id } = req.body;
+
+  if (
+    (amount !== undefined && typeof amount !== "number") ||
+    (timestamp !== undefined && typeof timestamp !== "string") ||
+    (description !== undefined && typeof description !== "string") ||
+    (category_id !== undefined && typeof category_id !== "number")
+  ) {
+    return res.status(400).json({ error: "Invalid request body format." });
+  }
+
+  let query = "UPDATE expense SET";
+  const updateFields = [];
+
+  if (amount !== undefined) {
+    updateFields.push(`amount='${amount}'`);
+  }
+
+  if (description !== undefined) {
+    updateFields.push(`description='${description}'`);
+  }
+
+  if (timestamp !== undefined) {
+    updateFields.push(`timestamp='${timestamp}'`);
+  }
+  if (category_id !== undefined) {
+    updateFields.push(`timestamp='${category_id}'`);
+  }
+  query += updateFields.join(", ");
+  query += ` WHERE id='${expenseId}'`;
+
+  try {
+    await run_query(query);
+    res.status(200).json({ message: "Expense updated successfully." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
-server.get("/expense/:id/", (req, res) => {
-  const query =
-    "SELECT * from app1_expense LEFT join app1_category on app1_expense.category_id = app1_category.id WHERE app1_expense = ";
-  res.status(200).json();
-});
+server.delete("/expense/:id/", async (req, res) => {
+  const expenseId = req.params.id;
 
-server.delete("/expense/:id", (req, res) => {
-  "SELECT * from app1_expense WHERE app1_expense = ";
-  res.status(204).end();
-});
+  const query = `DELETE FROM expense WHERE id = ${expenseId}`;
 
-server.patch("/expense/:id", (req, res) => {
-  const query = "UPDATE app1_expense SET amount='203' WHERE 'id'=1";
-  res.status(200).json();
+  try {
+    await run_query(query);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 server.all("*", (req, res) => {
-  res.status(404).json({ detail: "Response not found" });
+  res.status(404).json({ error: "Response not found" });
 });
 
-server.listen(5000, () => {
-  console.log("PORT: 5000");
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(`PORT: ${process.env.SERVER_PORT}`);
 });
